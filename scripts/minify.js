9ab87d29cb979c03b95f17034b3d21cf0773802a
Finnvb <finnvb@gmail.com>
2022-05-13 14:20:57 +0200

    minify, caching, compression

---
+++ scripts/minify.js
@@ -0,0 +1,34 @@
+const postcss = require('postcss')
+const cssnano = require('cssnano')
+const autoprefixer = require('autoprefixer')
+
+const fs = require('fs')
+
+const cssFileMain = 'public/style.css'
+const minCssFile = 'public/style.min.css'
+const cssFileLogin = 'public/loginStyles.css'
+const mincssFileLogin = 'public/loginStyles.min.css'
+
+// Minify css
+fs.readFile(cssFileMain, async (err, data) => {
+  const output = await postcss([cssnano, autoprefixer]).process(data);
+  const minifiedCss = output.css;
+ 
+  fs.writeFile(minCssFile, minifiedCss, err => {
+    if (err) {
+      console.log(err);
+    }
+  })
+})
+
+
+fs.readFile(cssFileLogin, async (err, data) => {
+  const output = await postcss([cssnano, autoprefixer]).process(data);
+  const minifiedCss = output.css;
+ 
+  fs.writeFile(mincssFileLogin, minifiedCss, err => {
+    if (err) {
+      console.log(err);
+    }
+  })
+})
